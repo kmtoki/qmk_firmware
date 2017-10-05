b9856c03caca64618dfcdb04180207d1d461f0ca
kmtoki <higumaido@gmail.com>
2017-10-05 19:29:52 +0900

    fix

--- keyboards/helix/keymaps/kmtoki/keymap.c
+++ keyboards/helix/keymaps/kmtoki/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,        KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,          KC_Y,          KC_U,      KC_I,    KC_O,    KC_P,    KC_BSPC, \
       CTL_T(KC_ESC), LT(MEDIA, KC_A),   KC_S,    KC_D,      KC_F,          KC_G,          KC_H,      KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT,       KC_Z,     KC_X,    KC_C,    KC_V,      KC_B,          KC_N,          KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_MINS, \
-      MO(ADJUST),    KC_LCTL,  KC_LALT, KC_LGUI, MO(LOWER), SFT_T(KC_SPC), SFT_T(KC_ENT), MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      MO(ADJUST),    KC_LCTL,  KC_LGUI, KC_LALT, MO(LOWER), SFT_T(KC_SPC), SFT_T(KC_ENT), MO(RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
